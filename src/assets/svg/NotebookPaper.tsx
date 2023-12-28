2ab6073754a6b269a3922e56ce172c76fe67df74
Valentinas <31697821+valentk777@users.noreply.github.com>
2023-12-28 08:53:00 +0200

    [#2] Fix images

---
+++ src/assets/svg/NotebookPaper.tsx
@@ -0,0 +1,180 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+import CustomSvgProps, { SetDefaultColorIfNull } from "../CustomSvgProps";
+
+const NotebookPaper = (props: CustomSvgProps) => {
+
+    const { backgroundColor, primaryColor, secondaryColor, borderColor } = SetDefaultColorIfNull(props);
+
+    return (
+        <Svg
+            viewBox="0 0 2807 1604"
+            {...props}
+        >
+            <Path
+                d="M3018.925 1813.772H-212.682V-210.203h3231.606v2023.975zm-3227.43-4.176h3223.254V-206.027H-208.505v2015.623z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1708.606H3016.836V1712.782H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1607.614H3016.836V1611.79H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1506.624H3016.836V1510.8H-210.594z"
+                fill={backgroundColor} />
+            <Path
+                d="M-210.594 1405.634H3016.836V1409.81H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1304.645H3016.836V1308.821H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1203.655H3016.836V1207.831H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1102.665H3016.836V1106.841H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 1001.676H3016.836V1005.8520000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 900.686H3016.836V904.8620000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 799.696H3016.836V803.8720000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path d="M-210.594 698.707H3016.836V702.883H-210.594z" fill={backgroundColor} />
+            <Path
+                d="M-210.594 597.715H3016.836V601.8910000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path d="M-210.594 496.725H3016.836V500.901H-210.594z" fill={backgroundColor} />
+            <Path d="M-210.594 395.735H3016.836V399.911H-210.594z" fill={backgroundColor} />
+            <Path
+                d="M-210.594 294.746H3016.836V298.92199999999997H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path d="M-210.594 193.756H3016.836V197.932H-210.594z" fill={backgroundColor} />
+            <Path
+                d="M-210.594 92.766H3016.836V96.94200000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 -8.223H3016.836V-4.047000000000001H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-210.594 -109.213H3016.836V-105.03699999999999H-210.594z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2916.948 -208.115H2921.124V1811.683H2916.948z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2819.147 -208.115H2823.323V1811.683H2819.147z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2721.344 -208.115H2725.52V1811.683H2721.344z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2623.544 -208.115H2627.72V1811.683H2623.544z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2525.743 -208.115H2529.919V1811.683H2525.743z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2427.942 -208.115H2432.118V1811.683H2427.942z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2330.142 -208.115H2334.3179999999998V1811.683H2330.142z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2232.341 -208.115H2236.517V1811.683H2232.341z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M2134.541 -208.115H2138.717V1811.683H2134.541z"
+                fill={backgroundColor}
+            />
+            <Path d="M2036.74 -208.115H2040.916V1811.683H2036.74z" fill={backgroundColor} />
+            <Path d="M1938.94 -208.115H1943.116V1811.683H1938.94z" fill={backgroundColor} />
+            <Path
+                d="M1841.136 -208.115H1845.312V1811.683H1841.136z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1743.336 -208.115H1747.512V1811.683H1743.336z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1645.535 -208.115H1649.711V1811.683H1645.535z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1547.734 -208.115H1551.9099999999999V1811.683H1547.734z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1449.934 -208.115H1454.11V1811.683H1449.934z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1352.133 -208.115H1356.309V1811.683H1352.133z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1254.333 -208.115H1258.509V1811.683H1254.333z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1156.532 -208.115H1160.7079999999999V1811.683H1156.532z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M1058.731 -208.115H1062.907V1811.683H1058.731z"
+                fill={backgroundColor}
+            />
+            <Path d="M960.928 -208.115H965.104V1811.683H960.928z" fill={backgroundColor} />
+            <Path d="M863.127 -208.115H867.303V1811.683H863.127z" fill={backgroundColor} />
+            <Path d="M765.327 -208.115H769.503V1811.683H765.327z" fill={backgroundColor} />
+            <Path
+                d="M667.527 -208.115H671.7030000000001V1811.683H667.527z"
+                fill={backgroundColor}
+            />
+            <Path d="M569.726 -208.115H573.902V1811.683H569.726z" fill={backgroundColor} />
+            <Path d="M471.925 -208.115H476.101V1811.683H471.925z" fill={backgroundColor} />
+            <Path d="M374.124 -208.115H378.3V1811.683H374.124z" fill={backgroundColor} />
+            <Path d="M276.324 -208.115H280.5V1811.683H276.324z" fill={backgroundColor} />
+            <Path d="M178.524 -208.115H182.7V1811.683H178.524z" fill={backgroundColor} />
+            <Path d="M80.72 -208.115H84.896V1811.683H80.72z" fill={backgroundColor} />
+            <Path
+                d="M-17.08 -208.115H-12.903999999999998V1811.683H-17.08z"
+                fill={backgroundColor}
+            />
+            <Path
+                d="M-114.881 -208.115H-110.705V1811.683H-114.881z"
+                fill={backgroundColor}
+            />
+        </Svg>
+    )
+}
+
+export default NotebookPaper
